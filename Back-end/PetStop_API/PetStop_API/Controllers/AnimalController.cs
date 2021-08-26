4f0130c7228f83d59c7b4ee4797964304792b49f
Victor Oliveira <victor.souza.41@hotmail.com>
2021-08-25 21:18:39 -0300

    Implementado controller e documentação temporária

--- Back-end/PetStop_API/PetStop_API/Controllers/AnimalController.cs
+++ Back-end/PetStop_API/PetStop_API/Controllers/AnimalController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetStop_API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,41 @@ using System.Threading.Tasks;
 
 namespace PetStop_API.Controllers
 {
-    public class AnimalController : Controller
+    [ApiController]
+    [Route("/api/animal")]
+    public class AnimalController : ControllerBase
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Cadastrar um Pet
+        /// </summary>
+        /// <remarks>
+        /// Requisição de exemplo:
+        /// {
+        ///     "Nome": "Snoop",
+        ///     "Data Nascimento": "01-01-2000",
+        ///     "Email": "dono@gmail.com",
+        ///     "CEP": "12345678",
+        ///     "Endereco": "Rua Primeiro de Janeiro",
+        ///     "Cidade": "Belo Horizonte",
+        ///     "Bairro": "Centro",
+        ///     "Complemento": "Casa"
+        /// }
+        /// </remarks>
+        /// <param name="model">Dados de um novo Pet</param>
+        /// <returns>Objeto recém criado</returns>
+        /// <response code="201">Pet cadastrado com sucesso</response>
+        /// <response code="400">Informação inválida</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post([FromBody] Animal animal)
         {
-            return View();
+            Animal pet = new Animal(animal.Nome, animal.DataNascimento, animal.Email, animal.Cep, animal.Endereco, animal.Bairro, animal.Complemento, animal.Cidade);
+
+            //_dbContext.Carros.Add(carro);
+            //_dbContext.SaveChanges();
+
+            return Created("OK", pet);
         }
     }
 }
--- Back-end/PetStop_API/PetStop_API/Models/Animal.cs
+++ Back-end/PetStop_API/PetStop_API/Models/Animal.cs
@@ -7,5 +7,24 @@ namespace PetStop_API.Models
 {
     public class Animal
     {
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Cep { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public Animal(string Nome, string Data_Nascimento, string Email, string Cep, string Endereco, string Bairro, string Complemento, string Cidade) 
+        {
+            this.Nome = Nome;
+            this.DataNascimento = Data_Nascimento;
+            this.Email = Email;
+            this.Bairro = Bairro;
+            this.Cep = Cep;
+            this.Endereco = Endereco;
+            this.Complemento = Complemento;
+            this.Cidade = Cidade;
+        }
     }
 }
