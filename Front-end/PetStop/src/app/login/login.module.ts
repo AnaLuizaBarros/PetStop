1cf9680b9aeba275eb355416cf14853011c35068
AnaLuizaBarros <analuizagbarros@gmail.com>
2021-11-25 18:51:51 -0300

    fix

--- Front-end/PetStop/src/app/login/login.module.ts
+++ Front-end/PetStop/src/app/login/login.module.ts
@@ -4,29 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login.component';
-import {
-  AuthService,
-  AuthServiceConfig,
-  FacebookLoginProvider,
-  GoogleLoginProvider,
-  SocialLoginModule,
-} from 'angular-6-social-login';
-import { environment } from 'src/environments/environment';
 
-// Configs
-export function getAuthServiceConfigs() {
-  const config = new AuthServiceConfig([
-    {
-      id: FacebookLoginProvider.PROVIDER_ID,
-      provider: new FacebookLoginProvider(environment.fbAppId),
-    },
-    {
-      id: GoogleLoginProvider.PROVIDER_ID,
-      provider: new GoogleLoginProvider(environment.googleClientId),
-    },
-  ]);
-  return config;
-}
 @NgModule({
   imports: [
     CommonModule,
@@ -38,12 +16,8 @@ export function getAuthServiceConfigs() {
   exports: [LoginComponent],
   declarations: [LoginComponent],
   entryComponents: [LoginComponent],
-  providers: [
-    LoginService,
-    /* {
-      provide: AuthServiceConfig,
-      useFactory: getAuthServiceConfigs,
-    },*/
-  ],
+  providers: [LoginService],
 })
-export class LoginModule {}
+export class LoginModule {
+  constructor() {}
+}
